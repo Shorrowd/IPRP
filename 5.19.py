bd4cadfa8af2c6f38bf6ddb67639fc71bc596efc
Shorrowd <tomasamaropt2005@gmail.com>
2023-10-29 22:18:08 +0000

    .

--- 5.19.py
+++ 5.19.py
@@ -1,24 +1,26 @@
 import turtle as t
 import random 
 
-t.speed(20)
+t.speed(50)
+t.bgcolor("black")
+
 dimensao = int(input("Introduz uma dimensão:"))
-celula = int(input("Introduz a dimensão de cada célula:"))
-conta = dimensao/celula
+conta = dimensao / 8
 conta_inteira = int(conta)
 avanco = conta % 1
 
-def quadrado(dimensao,celula,conta_inteira):
+def quadrado(dimensao,conta):
+    t.color("white")
     t.penup()
     t.setposition(-dimensao/2,dimensao/2)
     t.pendown()
-    
+
     for i in range(4):
         t.forward(dimensao)
         t.right(90)
 
-    for i in range(conta_inteira):
-        t.forward(celula)
+    for i in range(8):
+        t.forward(conta)
         t.right(90)
         t.forward(dimensao)
         t.back(dimensao)
@@ -26,8 +28,8 @@ def quadrado(dimensao,celula,conta_inteira):
 
     t.right(90)
 
-    for i in range(conta_inteira):
-        t.forward(celula)
+    for i in range(8):
+        t.forward(conta)
         t.right(90)
         t.forward(dimensao)
         t.back(dimensao)
@@ -36,34 +38,81 @@ def quadrado(dimensao,celula,conta_inteira):
 if (avanco != 0):
     print("Medidas Erradas")
 else:
-    quadrado(dimensao,celula,conta_inteira)
+    quadrado(dimensao,conta)
 
 t.penup()
 t.goto(0,0)
 t.pendown()
-t.width(1)
 t.shape("circle")
 t.color("red")
 t.stamp()
 t.shape("classic")
 
-
 def passeio():
-    num = random.randint(10,30)
+    num = random.randint(50,100)
     for i in range(num):
         head = random.randint(1,4)
-        if head == 1:
+        if (head == 1):
             t.setheading(0)
-        elif head == 2:
+        elif (head == 2):
             t.setheading(90)
-        elif head == 3:
+        elif (head == 3):
             t.setheading(180)
-        elif head == 4:
+        elif (head == 4):
             t.setheading(270)
-        t.pencolor("red")
-        t.forward(dimensao/8)
+
+        t.forward(conta)
 
 passeio()
+
+if t.xcor() >= dimensao/2:
+    head = random.randint(1,3)
+    if (head == 1):
+            t.setheading(90)
+            t.fd(conta)
+    elif (head == 2):
+            t.setheading(180)
+            t.fd(conta)
+    elif (head == 3):
+            t.setheading(270)
+            t.fd(conta)
+    print("Passou da Grelha. ")
+elif t.xcor() <= -dimensao/2:
+    head = random.randint(1,3)
+    if (head == 1):
+            t.setheading(90)
+            t.fd(conta)
+    elif (head == 2):
+            t.setheading(270)
+            t.fd(conta)
+    elif (head == 3):
+            t.setheading(0)
+            t.fd(conta)
+    print("Passou da Grelha. ")
+elif t.ycor() <= -dimensao/2:
+    head = random.randint(1,3)
+    if (head == 1):
+            t.setheading(90)
+            t.fd(conta)
+    elif (head == 2):
+            t.setheading(180)
+            t.fd(conta)
+    elif (head == 3):
+            t.setheading(0)
+            t.fd(conta)
+    print("Passou da Grelha. ")
+elif t.ycor() >= dimensao/2:
+    head = random.randint(1,3)
+    if (head == 1):
+            t.setheading(270)
+            t.fd(conta)
+    elif (head == 2):
+            t.setheading(180)
+            t.fd(conta)
+    elif (head == 3):
+            t.setheading(0)
+            t.fd(conta)
+    print("Passou da Grelha. ")
 
 t.end_fill()
 t.exitonclick()
