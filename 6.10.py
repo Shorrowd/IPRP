18a272311d3f490ee4f6fa0981031c213cac6ab5
Tomás Amaro <tomasamaropt2005@gmail.com>
2023-11-16 14:30:38 +0000

    .

--- 6.10.py
+++ 6.10.py
@@ -1,1 +1,23 @@
-"autor = {"php":"Rasmus Lerdorf","perl":"Larry Wall","tcl":"John Ousterhout","awk":"Brian Kerninghan","java":"James Gosling","parrot":"Simon Cozens","python":"GuidovanRossum","xpto":"zxcv"}
+autor = {"php":"Rasmus Lerdorf","perl":"Larry Wall","tcl":"John Ousterhout","awk":"Brian Kerninghan","java":"James Gosling","parrot":"Simon Cozens","python":"GuidovanRossum","xpto":"zxcv",}
+autor2 ={"casa":"home"}
+Guido_van_Rossum = {}
+x = 0
+
+autor.update(autor2)
+print ("Autor update: ", autor)
+
+autor["python"] = "Guido van Rossum"
+print("Novo autor: ", autor)
+
+del autor ['xpto']
+print ("Sem xpto: ", autor)
+
+for elem in sorted(autor.keys()):
+    x += 1
+   
+print ("Existem ",x, "elementos")
+
+if 'c++'  in autor:
+    print("Existe uma entrada")
+else:
+    print("Não existe uma entrada")
---
+++ 6.14.py
@@ -0,0 +1,18 @@
+pessoas = {
+    662023:{
+        "sexo":"M","idade":50,"peso":60,"altura": 1.70}
+}
+
+def metabolismo(pacientes):
+    output = dict()
+    for paciente in pacientes:
+        if pacientes[paciente] ["sexo"] == "M":
+            pacientes[paciente] = 66 + (6.3*pacientes[paciente])
+            ["peso"] + (12.9*pacientes[paciente])
+            ["altura"] - (6.8*pacientes[paciente])
+            ["idade"]
+
+        else:
+            pacientes[paciente]["basal"] = 65.5 + (4.3*pacientes[paciente] ["peso"]) (4.7*pacientes[paciente]["altura"])-(4.7*pacientes[paciente]["idade"])
+        
+metabolismo(pacientes)
