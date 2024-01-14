a81f0e502aa8b12b72ac0bc829f353373f4aae87
Shorrowd <tomasamaropt2005@gmail.com>
2024-01-14 18:17:23 +0000

    .

--- Teste 2019 4.py
+++
@@ -1,8 +0,0 @@
-def publicacoes_conjuntas(ficheiro,nome):
-    lista = []
-    ficheiro_nomes = open(ficheiro, "r")
-    for nome in ficheiro_nomes:
-        print(nome)
-
-
-print(publicacoes_conjuntas("ficheiro.txt","Pinto"))
